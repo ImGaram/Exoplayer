beefc1ecb3b91961acb8615979e6bfdf8688cd66
imgaram <s21049@gsm.hs.kr>
2022-09-03 18:55:32 +0900

    exoplayer 예제1

--- app/src/main/java/com/example/exoplayer/MainActivity.kt
+++ app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.exoplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.exoplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var sampleUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+
+    var player: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mediaItem = MediaItem.fromUri(Uri.parse(sampleUrl))
+        player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoplayer ->
+                binding.exoPlayer.player = exoplayer
+            }
+
+        player!!.setMediaItem(mediaItem)
+
+        player!!.prepare()
+        player!!.play()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player!!.pause()
+        player!!.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player!!.stop()
+        player!!.playWhenReady = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player!!.release()
     }
 }
